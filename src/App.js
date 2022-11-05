ddb17bcc750f2a98459077e291a4335c8287dc2a
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2022-11-05 13:59:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='main'>
+      <div className='header'>
+        <h1 className='heading'>MRC</h1>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
